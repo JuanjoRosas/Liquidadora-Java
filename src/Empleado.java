8cb834d49a0cbc5d340b04686250ec6f69e8e073
Juan Rosas <jjuan_2001@hotmail.com>
2023-03-24 15:21:45 -0500

    Primer commmit con la clase de empleado ya creada.

---
+++ src/Empleado.java
@@ -0,0 +1,92 @@
+import java.util.Date;
+
+/*
+ * Esta clase representa a un empleado de la empresa. En este caso se asume que el empleado
+ * no tiene ha tenido ascensos y no da la posibilidad de otorgarle uno tampoco.
+ */
+public class Empleado {
+  //ATRIBUTOS
+  /*
+   * Nombre y apellidos de empleado.
+   */
+  private String nombre;
+
+  /*
+   * Cadena de texto con el código de identificación del empleado.
+   */
+  private String identificacion;
+
+  /*
+   * Nombre del cargo.
+   */
+  private String cargo;
+
+  /*
+   * Salario mensual del empleado.
+   */
+  private double salario;
+
+  /*
+   * Fecha en la que ingreso a la compañia
+   */
+  private Date fechaIngreso;
+
+  //CONSTRUCTOR
+  /**
+   * Crea una instancia de la clase Empleado con los valores pasados por parametro.
+   * @param pNombre Cadena de texto que se va a asignar al nombre del empleado.
+   * @param pId Cadena de texto que se va a asignar a la identificacion del empleado.
+   * @param pCargo Cadena de texto que se va a asignar al nombre del cargo del empleado.
+   * @param pSalario Valor numerico que se va a asignar al salario mensual del empleado.
+   * @param pFechaIngreso Fecha que se va a asignar a la fecha de ingreso del empleado.
+   */
+  public Empleado(String pNombre, String pId, String pCargo, double pSalario, Date pFechaIngreso){
+    nombre = pNombre;
+    identificacion = pId;
+    cargo = pCargo;
+    salario = pSalario;
+    fechaIngreso = pFechaIngreso;
+  }
+
+  //METODOS
+  /**
+   * Retorna el nombre del empleado.
+   * @return Cadena de texto con el nombre del empleado.
+   */
+  public String getNombre() {
+    return nombre;
+  }
+
+  /**
+   * Retorna la identificación del empleado.
+   * @return Cadena de texto con la identificación del empleado.
+   */
+  public String getId() {
+    return identificacion;
+  }
+
+  /**
+   * Retorna el nombre del cargo del empleado.
+   * @return Cadena de texto con el nombre del cargo del empleado.
+   */
+  public String getCargo() {
+    return cargo;
+  }
+
+/**
+ * Retorna el valor del salario mensual del empleado.
+ * @return double con el valor del salario mensual del empleado.
+ */
+  public double getSalario() {
+    return salario;
+  }
+
+  /**
+   * Retorna la fecha de ingreso del empleado.
+   * @return Date con la fecha de ingreso del empleado.
+   */
+  public Date getFechaIngreso() {
+    return fechaIngreso;
+  }
+
+}
