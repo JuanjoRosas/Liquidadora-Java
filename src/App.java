7bad7ffa55c759e21788871ff9a51d3d02983125
Oscar Cáceres <91705340+osfecasu025@users.noreply.github.com>
2023-03-24 21:08:14 -0500

    ultima modificacion de App y arreglo de cargo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,142 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //ArrayList<Empleado> listaEmpleados = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean agregarEmpleado = true;
+        SimpleDateFormat dateF = new SimpleDateFormat("dd/MM/yyyy");
+        
+        while(agregarEmpleado){
+            System.out.println("Ingrese el nombre del empleado:");
+            String nombre = reader.readLine();   
+            System.out.println("Ingrese la identificacion del empleado:");
+            String identificacion = reader.readLine();  
+            Date fecha;
+            while(true){
+                System.out.println("");
+                System.out.println("Ingrese la fecha de ingreso formato dd/MM/AAAA (ej: '03/02/2012'):");
+                String fechaIngreso = reader.readLine();
+                
+                try {
+                    fecha=dateF.parse(fechaIngreso);
+                    System.out.println(">formato de fecha correta!.");
+                    break;
+                } catch (Exception e) {
+                    System.out.println(">El formato de la fecha no es el indicado.");
+                }  
+            }
+            while(true){
+                System.out.println("Ingrese el cargo del empleado:");
+                String cargos = reader.readLine();
+                try {
+                    Cargo cargo = null;//reemplazar por la funcion de empresa                  
+                    break;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }  
+            }
+
+            while(true){
+                double salario = 0;
+                try {
+                    System.out.println("Ingrese el salario basico del empleado:");
+                    String aux = reader.readLine();
+                    salario = Double.parseDouble(aux);                  
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                if(salario!=0){
+                    System.out.println("Salario admitido");
+                }else{
+                    try {
+                        //llamar funcion de verificacion salario por cargo              
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }  
+                }
+            }
+            //agregar empleado
+            System.out.println("¿Desea agregar otro empleado? (s/n)");
+            String mensaje = reader.readLine();
+            agregarEmpleado = mensaje.equalsIgnoreCase("s");
+        }
+        boolean salir = false;
+        while (!salir) {
+            
+            System.out.println("Seleccione una opcion:"
+                    + "\n 1.Generar nomina de un empleado."
+                    + "\n 2.Generar nomina a todos los empleado."
+                    + "\n 3.Salir.");
+            int opcion = reader.readInt();
+            switch (opcion) {
+                case "1":
+                    System.out.println("Ingrese la identificacion del empleado al cual desea generar nomina:");
+                    String mensaje = reader.readLine();
+                     Empleado empleado = null//aqui va el metodo de empresa donde se hace la busqueda del empleado
+                    try {
+                    if (empleado != null) {
+                        System.out.println("Ingrese el mes correspondiente (ENERO-DICIEMBRE):"
+                                + "\n 1.Enero."
+                                + "\n 2.Febrero."
+                                + "\n 3.Marzo."
+                                + "\n 4.Abril."
+                                + "\n 5.Mayo."
+                                + "\n 6.Junio."
+                                + "\n 7.Julio."
+                                + "\n 8.Agosto."
+                                + "\n 9.Septiembre."
+                                + "\n 10.Octubre."
+                                + "\n 11.Noviembre."
+                                + "\n 12.Diciembre. ");
+                        String mes = reader.readLine();
+                        }   
+
+                
+                    }   catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        } 
+                    break;
+
+                case "2":
+                    //liquidar todos los empleados
+                    Empleado empleado = null//aqui va el metodo de empresa donde se hace la busqueda del empleado
+                    try {
+                    if (empleado != null) {
+                        System.out.println("Ingrese el mes correspondiente (ENERO-DICIEMBRE):"
+                                + "\n 1.Enero."
+                                + "\n 2.Febrero."
+                                + "\n 3.Marzo."
+                                + "\n 4.Abril."
+                                + "\n 5.Mayo."
+                                + "\n 6.Junio."
+                                + "\n 7.Julio."
+                                + "\n 8.Agosto."
+                                + "\n 9.Septiembre."
+                                + "\n 10.Octubre."
+                                + "\n 11.Noviembre."
+                                + "\n 12.Diciembre. ");
+                        String mes = reader.readLine();
+                        }   
+
+                
+                    }   catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        } 
+                    break;
+                case "3":
+                    salir = true;
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida.");
+            }
+
+        }
     }
 }
--- src/Cargo.java
+++ src/Cargo.java
@@ -5,25 +5,27 @@ public enum Mes {
   //------------------------------------------------
   //Elementos
   //------------------------------------------------
-AdministradoraNegocio("ADMINISTRADORA_NEGOCIO", 1500000, 1959999.99 );
-Vendedora("VENDEDORA", 1300000, 1483299.99);
-Operario("OPERARIO", 1000000, 1200999.99);
-Cajero("CAJERO", 1100000, 1199999.99);
+AdministradoraNegocio("ADMINISTRADORA_NEGOCIO", 1500000, 1959999.99),
+Vendedora("VENDEDORA", 1300000, 1483299.99),
+Operario("OPERARIO", 1000000, 1200999.99),
+Cajero("CAJERO", 1100000, 1199999.99),
 LiquidadoraNomina("LIQUIDADORA_NOMINA", 1200000, 1299999.99);
 
 private String nombreCargo;
 private double salarioInf;
 private double salarioSup;
 
-@param nombreCargo
-@param salarioInf
-@param salarioSup
+/**
+ * @param nombreCargo
+ * @param salarioInf
+ * @param salarioSup
+ */
 
 private Cargo(String nombreCargo, double salarioInf, double salarioSup){
 
     this.nombreCargo = nombreCargo;
     this.salarioInf = salarioInf;
-    this.salarioSup = salarioS
+    this.salarioSup = salarioSup;
 
 }
 
